0744eabcbff9d59be23087d22339ba2851862d3a
khemraj <khemraj@khemrajs-MacBook-Air.local>
2024-09-22 06:44:49 +0800

    chore: fixed create task

--- frontend/src/components/queries/fetchTasks.jsx
+++ frontend/src/components/queries/fetchTasks.jsx
@@ -49,11 +49,14 @@ function Tasks() {
       ) : null}
 
       <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-        <h1 class="text-3xl text-center font-semibold mt-6">Browse</h1>
-        <div className="flex flex-wrap justify-center items-center gap-12 flex-col sm:flex-row">
+        <h1 className="text-3xl text-center font-semibold mt-6">Browse</h1>
+        <div className=" gap-12 flex-col sm:flex-row">
           {data?.taskCreateds?.map((q) => (
+            console.log(q),
             <TaskCard
-              id={q.taskId}
+              key={q.id} 
+              id={q.id}
+              taskId={q.taskId}
               title={q.title}
               description={q.description}
               taskType={q.taskType}

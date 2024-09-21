f9025383ef4cfdd91113fb5b8cfed03747ed136a
Allard <AllardQuek@users.noreply.github.com>
2024-09-21 19:46:06 +0800

    Add header to layout

--- frontend/src/components/TaskifyProfile.jsx
+++ frontend/src/components/TaskifyProfile.jsx
@@ -1,145 +1,103 @@
-import React, { useState }  from 'react';
-import { ChevronDown } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
-import {EditableInput} from '../ui/EditableInput.js';
-import {
-  DynamicContextProvider,
-  DynamicWidget,
-} from "@dynamic-labs/sdk-react-core";
-import { EthersExtension } from "@dynamic-labs/ethers-v5";
-import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import axios from 'axios';
-import useWalletStore from '../store/wallet.jsx';
+import { ChevronDown } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { EditableInput } from "../ui/EditableInput.js";
+import Layout from "./Layout";
+import axios from "axios";
+import useWalletStore from "../store/wallet.jsx";
+
 const TaskifyProfile = () => {
   const navigate = useNavigate();
-
-  const setWalletClient = useWalletStore((state) => {
-    console.log('state: ', state);
-    return state.setWalletClient
-  });
-  const setPublicClient = useWalletStore((state) => state.setPublicClient);
-
-
   const handleClick = async () => {
-    const authToken = localStorage.getItem('dynamic_authentication_token');
-    console.log('sending token to backend' , authToken);
+    const authToken = localStorage.getItem("dynamic_authentication_token");
+    console.log("sending token to backend", authToken);
 
-    const response = await axios.post('http://localhost:8000/api/signUpOrSignIn',{
-      token:  authToken// Send proof in request body
-       // Send email in request body
-    });
+    const response = await axios.post(
+      "http://localhost:8000/api/signUpOrSignIn",
+      {
+        token: authToken, // Send proof in request body
+        // Send email in request body
+      }
+    );
     if (response.status == 200) {
       const data = response.data;
-      localStorage.setItem("email", data.email );
-      navigate('/verify');
+      localStorage.setItem("email", data.email);
+      navigate("/verify");
     } else {
       console.log("error occurred while calling api");
     }
-    console.log('data from token', response);
-    
-   
-  }
+    console.log("data from token", response);
+  };
 
   return (
-    <div className="relative flex size-full min-h-screen flex-col bg-slate-50 group/design-root overflow-x-hidden font-['Manrope','Noto_Sans',sans-serif]">
-      <div className="layout-container flex h-full grow flex-col">
-        <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#e7eef3] px-10 py-3">
-          <div className="flex items-center gap-4 text-[#0d161b]">
-            <div className="size-4">
-              <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M42.4379 44C42.4379 44 36.0744 33.9038 41.1692 24C46.8624 12.9336 42.2078 4 42.2078 4L7.01134 4C7.01134 4 11.6577 12.932 5.96912 23.9969C0.876273 33.9029 7.27094 44 7.27094 44L42.4379 44Z"
-                  fill="currentColor"
-                />
-              </svg>
-            </div>
-            <h2 className="text-[#0d161b] text-lg font-bold leading-tight tracking-[-0.015em]">EasyTasks</h2>
-          </div>
-          <div className="flex flex-1 justify-end gap-8">
-            <div className="flex items-center gap-9">
-              <a className="text-[#0d161b] text-sm font-medium leading-normal" href="#">Home</a>
-              <a className="text-[#0d161b] text-sm font-medium leading-normal" href="#">Browse tasks</a>
-              <a className="text-[#0d161b] text-sm font-medium leading-normal" href="#">Post a task</a>
-              <a className="text-[#0d161b] text-sm font-medium leading-normal" href="#">Earn money</a>
-              <a className="text-[#0d161b] text-sm font-medium leading-normal" href="#">Help</a>
-              
-              <DynamicContextProvider
-      settings={{
-        environmentId: "ed25802a-53aa-4165-9407-2906d615c0cd",
-        walletConnectorExtensions: [EthersExtension],
-        walletConnectors: [EthereumWalletConnectors],
-        events: {
-          onAuthSuccess: async(args) => {
-            // console.log('onAuthSuccess was called', args);
-            const {primaryWallet, user} = args;
-            //create contract instance using ethers
-            
-            const publicClient = await primaryWallet.getPublicClient();
-            const walletClient = await primaryWallet.getWalletClient();
-            // console.log(setWalletClient, " < set wakket client");
-            setWalletClient(walletClient); // Check if this actually sets the wallet correctly
-            setPublicClient(publicClient);
-            
-            // Logging to ensure correct wallet data
-            console.log("Wallet Client: ", walletClient);
-            console.log("Public Client: ", publicClient);
-
-            // you can get the jwt by calling the getAuthToken helper function
-          }
-        }
-      }}
-    >
-      <DynamicWidget />
-
-    </DynamicContextProvider>
-            </div>
-          </div>
-        </header>
-        <div className="px-40 flex flex-1 justify-center py-5">
-          <div className="layout-content-container flex flex-col w-[512px] max-w-[512px] py-5 max-w-[960px] flex-1">
-            <h3 className="text-[#0d161b] tracking-light text-2xl font-bold leading-tight px-4 text-center pb-2 pt-5">Create your profile</h3>
-            <h3 className="text-[#0d161b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">Your name</h3>
-            <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-              <label className="flex flex-col min-w-40 flex-1">
-                <p className="text-[#0d161b] text-base font-medium leading-normal pb-2">First name</p>
-                <EditableInput/>
-              
-              </label>
-            </div>
-            <h3 className="text-[#0d161b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">What's your role?</h3>
-            <div className="flex flex-wrap gap-3 p-4">
-              <label className="text-sm font-medium leading-normal flex items-center justify-center rounded-xl border border-[#cfdee7] px-4 h-11 text-[#0d161b] has-[:checked]:border-[3px] has-[:checked]:px-3.5 has-[:checked]:border-[#209cee] relative cursor-pointer">
-                I need help
-                <input type="radio" className="invisible absolute" name="role" />
-              </label>
-              <label className="text-sm font-medium leading-normal flex items-center justify-center rounded-xl border border-[#cfdee7] px-4 h-11 text-[#0d161b] has-[:checked]:border-[3px] has-[:checked]:px-3.5 has-[:checked]:border-[#209cee] relative cursor-pointer">
-                I can help
-                <input type="radio" className="invisible absolute" name="role" />
-              </label>
-            </div>
-            <h3 className="text-[#0d161b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">Where do you live?</h3>
-            <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
-              <label className="flex flex-col min-w-40 flex-1">
-                <p className="text-[#0d161b] text-base font-medium leading-normal pb-2">Suburb, postcode or city</p>
-                <select className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0d161b] focus:outline-0 focus:ring-0 border border-[#cfdee7] bg-slate-50 focus:border-[#cfdee7] h-14 placeholder:text-[#4c7b9a] p-[15px] text-base font-normal leading-normal appearance-none">
-                  <option value="one"></option>
-                  <option value="two">two</option>
-                  <option value="three">three</option>
-                </select>
-                <ChevronDown className="pointer-events-none absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
-              </label>
-            </div>
-            <div className="flex px-4 py-3">
-              <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#209cee] text-slate-50 text-sm font-bold leading-normal tracking-[0.015em]"
-                onClick={handleClick}
-              >
-                <span className="truncate">Next</span>
-              </button>
+    <Layout>
+      <div className="relative flex size-full min-h-screen flex-col bg-slate-50 group/design-root overflow-x-hidden font-['Manrope','Noto_Sans',sans-serif]">
+        <div className="layout-container flex h-full grow flex-col">
+          <div className="px-40 flex flex-1 justify-center py-5">
+            <div className="layout-content-container flex flex-col w-[512px] max-w-[512px] py-5 max-w-[960px] flex-1">
+              <h3 className="text-[#0d161b] tracking-light text-2xl font-bold leading-tight px-4 text-center pb-2 pt-5">
+                Create your profile
+              </h3>
+              <h3 className="text-[#0d161b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">
+                Your name
+              </h3>
+              <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+                <label className="flex flex-col min-w-40 flex-1">
+                  <p className="text-[#0d161b] text-base font-medium leading-normal pb-2">
+                    First name
+                  </p>
+                  <EditableInput />
+                </label>
+              </div>
+              <h3 className="text-[#0d161b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">
+                What's your role?
+              </h3>
+              <div className="flex flex-wrap gap-3 p-4">
+                <label className="text-sm font-medium leading-normal flex items-center justify-center rounded-xl border border-[#cfdee7] px-4 h-11 text-[#0d161b] has-[:checked]:border-[3px] has-[:checked]:px-3.5 has-[:checked]:border-[#209cee] relative cursor-pointer">
+                  I need help
+                  <input
+                    type="radio"
+                    className="invisible absolute"
+                    name="role"
+                  />
+                </label>
+                <label className="text-sm font-medium leading-normal flex items-center justify-center rounded-xl border border-[#cfdee7] px-4 h-11 text-[#0d161b] has-[:checked]:border-[3px] has-[:checked]:px-3.5 has-[:checked]:border-[#209cee] relative cursor-pointer">
+                  I can help
+                  <input
+                    type="radio"
+                    className="invisible absolute"
+                    name="role"
+                  />
+                </label>
+              </div>
+              <h3 className="text-[#0d161b] text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">
+                Where do you live?
+              </h3>
+              <div className="flex max-w-[480px] flex-wrap items-end gap-4 px-4 py-3">
+                <label className="flex flex-col min-w-40 flex-1">
+                  <p className="text-[#0d161b] text-base font-medium leading-normal pb-2">
+                    Suburb, postcode or city
+                  </p>
+                  <select className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl text-[#0d161b] focus:outline-0 focus:ring-0 border border-[#cfdee7] bg-slate-50 focus:border-[#cfdee7] h-14 placeholder:text-[#4c7b9a] p-[15px] text-base font-normal leading-normal appearance-none">
+                    <option value="one"></option>
+                    <option value="two">two</option>
+                    <option value="three">three</option>
+                  </select>
+                  <ChevronDown className="pointer-events-none absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
+                </label>
+              </div>
+              <div className="flex px-4 py-3">
+                <button
+                  className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#209cee] text-slate-50 text-sm font-bold leading-normal tracking-[0.015em]"
+                  onClick={handleClick}
+                >
+                  <span className="truncate">Next</span>
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
--- frontend/src/components/VerifyIdentity.jsx
+++ frontend/src/components/VerifyIdentity.jsx
@@ -1,43 +1,41 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Layout from "../components/Layout";
 
-import { IDKitWidget, VerificationLevel } from '@worldcoin/idkit'
-import useWalletStore from '../store/wallet.jsx';
+import { IDKitWidget, VerificationLevel } from "@worldcoin/idkit";
+import useWalletStore from "../store/wallet.jsx";
 
-import apiService from '../service/apiService.js';
+import apiService from "../service/apiService.js";
 
 const VerifyIdentity = () => {
   const navigate = useNavigate();
-  const email = localStorage.getItem('email');
+  const email = localStorage.getItem("email");
   const wallet = useWalletStore((state) => state.getWallet());
   // console.log('wallet is: ', wallet, window.dynamicWalletClient);
 
-
   const handleClickButton = async () => {
-    console.log('button clicked');
+    console.log("button clicked");
     const response = await apiService.signInUser();
-  }
+  };
   const onSuccess = async (data) => {
     console.log("world id verification success", data);
     // Extract proof and email from the data object (adjust based on actual data structure)
-    
-    
-  
+
     try {
-      const response = await axios.post('http://localhost:8000/api/verifyProof', {
-        proof: data, // Send proof in request body
-         // Send email in request body
-         email
-      });
-  
+      const response = await axios.post(
+        "http://localhost:8000/api/verifyProof",
+        {
+          proof: data, // Send proof in request body
+          // Send email in request body
+          email,
+        }
+      );
 
-
-      
       // If the request is successful
-      console.log('Proof verification successful:', response.data);
+      console.log("Proof verification successful:", response.data);
       if (response.status == 200) {
-        navigate('/dashboard');
+        navigate("/dashboard");
       }
       // Handle success (e.g., show a success message or redirect the user)
     } catch (error) {
@@ -45,60 +43,64 @@ const VerifyIdentity = () => {
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.error('Proof verification failed:', error.response.data);
+        console.error("Proof verification failed:", error.response.data);
         // Handle failure (e.g., show an error message)
       } else if (error.request) {
         // The request was made but no response was received
-        console.error('No response received:', error.request);
+        console.error("No response received:", error.request);
         // Handle network errors
       } else {
         // Something happened in setting up the request that triggered an error
-        console.error('Error setting up the request:', error.message);
+        console.error("Error setting up the request:", error.message);
       }
     }
-
-
-
-  }
+  };
 
   const handleVerify = (proof) => {
     console.log("verifying the proof", proof);
-  }
+  };
 
   return (
-    <div className="relative flex size-full min-h-screen flex-col bg-slate-50 group/design-root overflow-x-hidden font-['Manrope','Noto_Sans',sans-serif]">
-      <div className="layout-container flex h-full grow flex-col">
-        <div className="px-40 flex flex-1 justify-center py-5">
-          <div className="layout-content-container flex flex-col w-[512px] max-w-[512px] py-5 max-w-[960px] flex-1">
-            <h1 className="text-[#0d161b] text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 text-left pb-3 pt-5">
-              Verify your identity to use EasyTasks
-            </h1>
-            <p className="text-[#0d161b] text-base font-normal leading-normal pb-3 pt-1 px-4">
-              EasyTasks is using Worldcoin to verify new users. This is a one-time process that will take 5 minutes.
-            </p>
-            <div className="flex px-4 py-3">
+    <Layout>
+      <div className="relative flex size-full min-h-screen flex-col bg-slate-50 group/design-root overflow-x-hidden font-['Manrope','Noto_Sans',sans-serif]">
+        <div className="layout-container flex h-full grow flex-col">
+          <div className="px-40 flex flex-1 justify-center py-5">
+            <div className="layout-content-container flex flex-col w-[512px] max-w-[512px] py-5 max-w-[960px] flex-1">
+              <h1 className="text-[#0d161b] text-[22px] font-bold leading-tight tracking-[-0.015em] px-4 text-left pb-3 pt-5">
+                Verify your identity to use EasyTasks
+              </h1>
+              <p className="text-[#0d161b] text-base font-normal leading-normal pb-3 pt-1 px-4">
+                EasyTasks is using Worldcoin to verify new users. This is a
+                one-time process that will take 5 minutes.
+              </p>
+              <div className="flex px-4 py-3">
+                <IDKitWidget
+                  app_id="app_staging_e7bc5518aa28f0a211351dbc5210d0ca" // obtained from the Developer Portal
+                  action="test-inifinite-verification" // this is your action id from the Developer Portal
+                  onSuccess={onSuccess} // callback when the modal is closed
+                  // handleVerify={handleVerify} // optional callback when the proof is received
+                  verification_level={VerificationLevel.Device}
+                >
+                  {({ open }) => (
+                    <button
+                      className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#209cee] text-slate-50 text-sm font-bold leading-normal tracking-[0.015em]"
+                      onClick={open}
+                    >
+                      <span className="truncate">Verify with World ID</span>
+                    </button>
+                  )}
+                </IDKitWidget>
 
-                  <IDKitWidget
-        app_id="app_staging_e7bc5518aa28f0a211351dbc5210d0ca" // obtained from the Developer Portal
-        action="test-inifinite-verification" // this is your action id from the Developer Portal
-        onSuccess={onSuccess} // callback when the modal is closed
-        // handleVerify={handleVerify} // optional callback when the proof is received
-        verification_level={VerificationLevel.Device}
-      >
-        {({ open }) => <button 
-        className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 flex-1 bg-[#209cee] text-slate-50 text-sm font-bold leading-normal tracking-[0.015em]"
-        
-        onClick={open}><span className="truncate">Verify with World ID</span></button>}
-      </IDKitWidget>
-
-    <button onClick={handleClickButton}> click this button sample</button>
-
-                
+                <button onClick={handleClickButton}>
+                  {" "}
+                  click this button sample
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 

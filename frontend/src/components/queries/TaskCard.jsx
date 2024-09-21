5dfdbb4ef2e64bc8bb4e70c75b2517b095d51008
Allard <AllardQuek@users.noreply.github.com>
2024-09-22 03:41:18 +0800

    Add task card

---
+++ frontend/src/components/queries/TaskCard.jsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+
+const TaskCard = ({ title, description, taskType, budget, isCreator }) => {
+  return (
+    <Card style={{ maxWidth: 345, margin: "20px auto" }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+        <FormGroup>
+          {parseInt(taskType) === 1 ? (
+            <FormControlLabel control={<Switch checked />} label="Online" />
+          ) : (
+            <FormControlLabel control={<Switch />} label="Online" />
+          )}
+        </FormGroup>
+        <Typography variant="body3" color="primary">
+          $
+          {budget.toLocaleString(undefined, {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}{" "}
+          USDC
+        </Typography>
+      </CardContent>
+
+      {!isCreator && (
+        <CardActions>
+          <Button size="small" color="success">
+            Accept
+          </Button>
+        </CardActions>
+      )}
+    </Card>
+  );
+};
+
+export default TaskCard;
